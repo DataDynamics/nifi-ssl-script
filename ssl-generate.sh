c8e542e64fefb5248758488cb7ab31ac8164957b
Edward KIM <fharenheit@gmail.com>
2023-09-10 16:38:05 +0900

    Update ssl-generate.sh

--- ssl-generate.sh
+++ ssl-generate.sh
@@ -7,25 +7,25 @@ CA_HOSTNAME=host1
 PASSWD=@123qwe@123qwe
 HOSTS=host1,host2
 
-if [ ! -d "$NIFI_SSL_HOME" ]; then
-  echo "$DIRECTORY does not exist."
-  mkdir $NIFI_SSL_HOME
+if [ ! -d "${NIFI_SSL_HOME}" ]; then
+  echo "${NIFI_SSL_HOME} does not exist."
+  mkdir ${NIFI_SSL_HOME}
 fi
 
-rm -rf $NIFI_SSL_HOME/*
+rm -rf ${NIFI_SSL_HOME}/*
 
 
 HOST_STRING=""
 for HOST in ${HOSTS//,/ }; do
-    HOST_STRING=$HOST_STRING" -n '$HOST'"
+    HOST_STRING=${HOST_STRING}" -n '${HOST}'"
 done
 
-echo $HOST_STRING
+echo ${HOST_STRING}
 
 sh ${NIFI_TOOLKIT_HOME}/bin/tls-toolkit.sh standalone \
-$HOST_STRING --days 3650 -c '$CA_HOSTNAME' \
---keyPassword $PASSWD  --trustStorePassword $PASSWD  --keyStorePassword $PASSWD \
--C 'CN=admin' --clientCertPassword $PASSWD \
--o $NIFI_SSL_HOME
+${HOST_STRING} --days 3650 -c '$CA_HOSTNAME' \
+--keyPassword ${PASSWD}  --trustStorePassword ${PASSWD}  --keyStorePassword ${PASSWD} \
+-C 'CN=admin' --clientCertPassword ${PASSWD} \
+-o ${NIFI_SSL_HOME}
 
-ls -lsa $NIFI_SSL_HOME
+ls -lsa ${NIFI_SSL_HOME}
